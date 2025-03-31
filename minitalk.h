6dee353b13b52e289e178a50367f2daafe86b09a
Erdemina <34918271+Erdemina@users.noreply.github.com>
2025-04-01 00:43:33 +0300

    start

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eulutas <eulutas@student.42istanbul.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/01 00:23:59 by eulutas           #+#    #+#             */
+/*   Updated: 2025/04/01 00:23:59 by eulutas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include "./printf/libftprintf.h"
+#include <unistd.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+#endif
